c0eb8a5101548ce2e42e566bf66cb4cbf4f85346
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-11-26 16:10:53 +0300

    spiralv2.1

--- Program.cs
+++ Program.cs
@@ -1,25 +1,25 @@
 ﻿
+
 Console.WriteLine("Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.\n" +
                 "Укажите размер массива количество строк и столбцов");
 int row = Convert.ToInt32(Console.ReadLine());
 int column = Convert.ToInt32(Console.ReadLine());
 
 int[,] array = new int[row, column];
-int numRow = array.GetLength(0), numColumn = array.GetLength(1);
-int upLeftCorner = 0, downRightCorner = 0, downLeftCorner = 0, upRightCorner = 0;
 int counter = 1;
-int i = 0;
-int j = 0;
+int x = 0;
+int y = 0;
+int corner = 0;
 
 Console.Clear();
-PrintSpiralW(array, upLeftCorner, upRightCorner, downRightCorner, downLeftCorner);
+PrintSpiralW(array, corner, y, x);
 
-void PrintSpiralW(int[,] array, int upLeftCorner, int upRightCorner, int downRightCorner, int downLeftCorner)
-{
-    numColumn = array.GetLength(1);
-    numRow = array.GetLength(0);
-    int sleep = 10;
-    const int width = 4;
+void PrintSpiralW(int[,] array, int corner, int i, int j)
+{  
+    int numColumn = array.GetLength(1);
+    int numRow = array.GetLength(0);
+    int sleep = 100;
+    const int width = 5;
     Console.CursorVisible = false;
     while (counter <= numColumn * numRow+1)
     {
@@ -28,30 +28,29 @@ void PrintSpiralW(int[,] array, int upLeftCorner, int upRightCorner, int downRig
         {
             Console.SetCursorPosition(0, numRow + 1);
             Console.Write($"Конец");
-            Console.ResetColor();
             return;
         }
-        if (i == upLeftCorner && j < numColumn - upRightCorner - 1)
+      
+        if (i == corner && j < numColumn - corner - 1)
         {
             if (j == 0 && i == 0)
             {
                 array[i, j] = counter;
                 Console.Write($"{array[i, j].ToString().PadLeft(width - 1, '0'),width}");
-                 counter++;
+                counter++;
             }
             j++;
             array[i, j] = counter;
             Console.Write($"{array[i, j].ToString().PadLeft(width - 1, '0'),width}");
         }
-        else if (j == numColumn - upRightCorner - 1 && i < numRow - downRightCorner - 1)
+        else if (j == numColumn - corner - 1 && i < numRow - corner - 1)
         {
             Console.SetCursorPosition(Console.CursorLeft - width, Console.CursorTop + 1);
-            Thread.Sleep(sleep);
             i++;
             array[i, j] = counter;
             Console.Write($"{array[i, j].ToString().PadLeft(width - 1, '0'),width}");
         }
-        else if (i == numRow - downRightCorner - 1 && j > downLeftCorner)
+        else if (i == numRow - corner - 1 && j > corner)
         {
             Console.SetCursorPosition(Console.CursorLeft - 2 * width, Console.CursorTop);
             j--;
@@ -67,9 +66,10 @@ void PrintSpiralW(int[,] array, int upLeftCorner, int upRightCorner, int downRig
         }
         counter++;
 
-        if ((i == upLeftCorner + 1) && (j == downLeftCorner) && (downLeftCorner != numColumn - upRightCorner - 1))
+        if ((i == corner + 1) && (j == corner) && (corner != numColumn - corner - 1))
         {
-            PrintSpiralW(array, upLeftCorner + 1, upRightCorner + 1, downRightCorner + 1, downLeftCorner + 1);
+            PrintSpiralW(array, corner + 1, i, j);
         }
     }
+    return;
 }
